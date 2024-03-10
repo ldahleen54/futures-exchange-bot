fbe5bd2bfb4c3892d65e01cc3abc9b18fe55076c
ldahleen54@gmail.com <ldahleen54@gmail.com>
2024-03-09 19:00:19 -0600

    Change to size method

--- index.ts
+++ index.ts
@@ -66,13 +66,13 @@ for (const folder of commandFolders) {
 
 (async () => {
 	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`)
+		console.log(`Started refreshing ${commands.size()} application (/) commands.`)
 		const data = await rest.put(
 			Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
 			{ body: commands }
 		)
 		console.log(`commands reloaded ${JSON.stringify(data)}`)
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`)
+		console.log(`Successfully reloaded ${data.size()} application (/) commands.`)
 	} catch (error) {
 		console.error(error)
 	}
